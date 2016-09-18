6fa34206d6304d1046df35dba47ecf3ddd39f98d
Haak, Jan <haakjan@amazon.com>
2016-09-18 17:35:55 +0800

    Breaking out to Boto

---
+++ update-alias.py
@@ -0,0 +1,17 @@
+import boto3
+import time
+
+#now that the tests have passed on the dev alias,
+#change the prod alias to be the same value as the dev alias
+func = "hello_there"
+client = boto3.client('lambda')
+devResponse = client.get_alias(FunctionName=func , Name='dev')
+print "Dev alias details: " + str(devResponse)
+devVersion = devResponse.get('FunctionVersion')
+print "Updating Production version to v " + str(devVersion)
+updateResponse = client.update_alias(
+    FunctionName=func,
+    Name='prod',
+    FunctionVersion=devVersion,
+    Description='Production Alias'
+)
