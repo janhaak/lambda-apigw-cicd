7c154525c22df61a9bfeac65749a687ecddedef4
Haak, Jan <haakjan@amazon.com>
2016-09-18 18:02:04 +0800

    unbreaking

--- lambda-python/hello.py
+++ lambda-python/hello.py
@@ -8,10 +8,6 @@ print('Loading function')
 
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-
-    this will break the build!!!!
-
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
