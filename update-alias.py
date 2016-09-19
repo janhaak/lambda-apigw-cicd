2b21584a7e237616909c4b6fa569467b8bf2948a
Jan Haak <jan@janhaak.com>
2016-09-19 10:11:04 +0800

    notes

--- update-alias.py
+++ update-alias.py
@@ -3,11 +3,17 @@ import time
 
 #now that the tests have passed on the dev alias,
 #change the prod alias to be the same value as the dev alias
+#lambda function name to update
 func = "hello_there"
+
 client = boto3.client('lambda')
+
+#get the version number that 'dev' alias points to:
 devResponse = client.get_alias(FunctionName=func , Name='dev')
 print "Dev alias details: " + str(devResponse)
 devVersion = devResponse.get('FunctionVersion')
+
+#update the 'prod' alias to the same version as 'dev'
 print "Updating Production version to v " + str(devVersion)
 updateResponse = client.update_alias(
     FunctionName=func,
