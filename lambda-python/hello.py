d174de04d0076554ee5de401edca59d86a8741c3
Jan Haak <jan@janhaak.com>
2016-09-20 09:20:26 +1000

    hello Brisbane

--- lambda-python/hello.py
+++ lambda-python/hello.py
@@ -4,11 +4,10 @@ import json
 
 print('Loading function')
 
-break the function
-asdffadsfas
+
 
 def lambda_handler(event, context):
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
+    return event['key1'] + "hello Brisbane!" # Echo back the first key value
