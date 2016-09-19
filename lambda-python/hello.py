77f4f0dc0e3d131d1ebaa8bd461929099245ab44
Jan Haak <jan@janhaak.com>
2016-09-19 16:29:02 +1000

    zoltaak

--- lambda-python/hello.py
+++ lambda-python/hello.py
@@ -10,4 +10,4 @@ def lambda_handler(event, context):
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
     #return event['key1']  # Echo back the first key value
-    return json.loads(event['key3'])
+    return json.loads(event)
